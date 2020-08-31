00280a716dbe20d2464e0c997c22691da051d727
Volodymyr Zavalskyi <vovazav172839@gmail.com>
2020-08-31 22:42:04 +0300

    Add docker build

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+APP="/var/www/html/api"
+sed -i -e "s|RewriteBase /|RewriteBase /api|g" "$APP/.htaccess"
+sed -i -e "s|'base_url'   => '/'|'base_url'   => '/api/'|g" "$APP/application/bootstrap.php"
+sed -i -e "s|localhost|$MYSQL_HOST|g" "$APP/application/config/database.php"
+
+exec "$@"
