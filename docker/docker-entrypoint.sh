c071ab439ab006778a96793dbb345cab634f67a1
Volodymyr Zavalskyi <vovazav172839@gmail.com>
2020-09-01 12:31:39 +0300

    gcp orb

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,5 +4,6 @@ APP="/var/www/html/api"
 sed -i -e "s|RewriteBase /|RewriteBase /api|g" "$APP/.htaccess"
 sed -i -e "s|'base_url'   => '/'|'base_url'   => '/api/'|g" "$APP/application/bootstrap.php"
 sed -i -e "s|localhost|$MYSQL_HOST|g" "$APP/application/config/database.php"
+#TODO db password sed / username
 
 exec "$@"
