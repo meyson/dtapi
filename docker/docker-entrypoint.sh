81ce9d9cc65557d3008814bcb268481902f3d61f
Volodymyr Zavalskyi <vovazav172839@gmail.com>
2020-09-02 22:04:37 +0300

    add context

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,7 +3,10 @@
 APP="/var/www/html/api"
 sed -i -e "s|RewriteBase /|RewriteBase /api|g" "$APP/.htaccess"
 sed -i -e "s|'base_url'   => '/'|'base_url'   => '/api/'|g" "$APP/application/bootstrap.php"
-sed -i -e "s|localhost|$MYSQL_HOST|g" "$APP/application/config/database.php"
-#TODO db password sed / username
+sed -i \
+  -e "s|mysql:host=localhost;dbname=dtapi2|mysql:$MYSQL_HOST;dbname=$MYSQL_DATABASE|g" \
+  -e "s|'username'   => 'dtapi'|'username'   => '$MYSQL_USER'|g" \
+  -e "s|'password'   => 'dtapi'|'password'   => '$MYSQL_PASSWORD'|g" \
+  "$APP/application/config/database.php"
 
 exec "$@"
