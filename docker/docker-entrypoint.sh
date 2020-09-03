6a46183726790ef9a775eb2b2b2b71ad6c17425b
Volodymyr Zavalskyi <vovazav172839@gmail.com>
2020-09-03 14:12:52 +0300

    fix sed

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,7 +4,7 @@ APP="/var/www/html/api"
 sed -i -e "s|RewriteBase /|RewriteBase /api|g" "$APP/.htaccess"
 sed -i -e "s|'base_url'   => '/'|'base_url'   => '/api/'|g" "$APP/application/bootstrap.php"
 sed -i \
-  -e "s|mysql:host=localhost;dbname=dtapi2|mysql:$MYSQL_HOST;dbname=$MYSQL_DATABASE|g" \
+  -e "s|mysql:host=localhost;dbname=dtapi2|mysql:host=$MYSQL_HOST;dbname=$MYSQL_DATABASE|g" \
   -e "s|'username'   => 'dtapi'|'username'   => '$MYSQL_USER'|g" \
   -e "s|'password'   => 'dtapi'|'password'   => '$MYSQL_PASSWORD'|g" \
   "$APP/application/config/database.php"
