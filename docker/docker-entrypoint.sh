ab5612c73592cd81a55efa4d83839f4c903f52bc
Volodymyr Zavalskyi <vovazav172839@gmail.com>
2020-09-04 13:17:10 +0300

    fix requests to root

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-APP="/var/www/html"
-
+APP="/var/www/html/api"
+sed -i -e "s|RewriteBase /|RewriteBase /api|g" "$APP/.htaccess"
+sed -i -e "s|'base_url'   => '/'|'base_url'   => '/api/'|g" "$APP/application/bootstrap.php"
 sed -i \
   -e "s|mysql:host=localhost;dbname=dtapi2|mysql:host=$MYSQL_HOST;dbname=$MYSQL_DATABASE|g" \
   -e "s|'username'   => 'dtapi'|'username'   => '$MYSQL_USER'|g" \
   -e "s|'password'   => 'dtapi'|'password'   => '$MYSQL_PASSWORD'|g" \
   "$APP/application/config/database.php"
 
+# The GKE Ingress controller won't work without index.html in root directory
+touch "/var/www/html/index.html"
 exec "$@"
